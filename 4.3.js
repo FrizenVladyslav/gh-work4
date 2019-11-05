ba61dc390a1d1fa602b9d1e3e17bb67437b91f93
FrizenVladyslav <FrizenVladyslav>
2019-11-06 00:12:21 +0200

    Update -  if with {}

--- 4.3.js
+++ 4.3.js
@@ -1,12 +1,19 @@
 function pow(x, n) {
   [x, n] = [+x, +n];
-  if (!Number.isInteger(n)) throw new Error(`${n} - not valid value`);
-  if (n === 0) return 1;
-  if (Number.isNaN(x) || -100 > x || x > 100)
+  if (!Number.isInteger(n)) {
+    throw new Error(`${n} - not valid value`);
+  }
+  if (n === 0) {
+    return 1;
+  }
+  if (Number.isNaN(x) || -100 > x || x > 100) {
     throw new Error(`${x} - not valid value`);
-
-  if (n < 0) return 1 / pow(x, -n);
-  else return +n === 1 ? x : x * pow(x, n - 1);
+  }
+  if (n < 0) {
+    return 1 / pow(x, -n);
+  } else {
+    return +n === 1 ? x : x * pow(x, n - 1);
+  }
 }
 
 console.log(pow(2.0, 10));
